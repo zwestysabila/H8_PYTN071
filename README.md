7b7f6e46317be3eebb1333f96675e5601d71ab9f
zwestysabila <zwestyts@gmail.com>
2022-01-11 21:59:23 +0700

    add Sesi 3 and README

---
+++ README.md
@@ -0,0 +1,3 @@
+Program Introduction To Python For Data Science
+Batch 075
+Zwesty Tria Sabila
---
+++ latihan01/mod.py
@@ -0,0 +1,10 @@
+s = "Hactiv8-PTP Python for Data Science"
+a = [100,200,300]
+
+def foo(arg):
+    print(f'arg = {arg}')
+
+class Foo:
+    pass
+    
+    
